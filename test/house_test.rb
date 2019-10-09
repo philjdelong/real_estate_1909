a37076fe288760397c8df0ca673507f99eb3aef8
Phil DeLong <philjdelong@gmail.com>
2019-10-09 09:33:06 -0600

    Initial commit

---
+++ test/house_test.rb
@@ -0,0 +1,65 @@
+require'minitest/pride'
+require'minitest/autorun'
+require'pry'
+require'./lib/room'
+require'./lib/house'
+
+class HouseTest < Minitest::Test
+  def setup
+    @house = House.new("$400000", "123 Sugar Lane")
+    @room_1 = Room.new(:bedroom, 10, 13)
+    @room_2 = Room.new(:bedroom, 11, 15)
+  end
+
+  def test_it_exists
+  skip
+
+  assert_instance_of House, @house
+  end
+
+  def test_it_has_a_price
+  skip
+
+  assert_equal "$400000", @house.price
+  end
+
+  def test_it_has_an_address
+  skip
+
+  assert_equal "123 Sugar Lane", @house.address
+  end
+
+  def test_it_has_rooms
+  skip
+
+  assert_equal [], @house.rooms
+  end
+
+  def test_it_can_add_rooms
+  skip
+
+  @room_1 = Room.new(:bedroom, 10, 13)
+  @room_2 = Room.new(:bedroom, 11, 15)
+  # binding.pry
+  assert_equal [room_1], @house.add_room(@room_1)
+  end
+
+  def test_it_can_add_rooms
+  skip
+
+  @room_1 = Room.new(:bedroom, 10, 13)
+  @room_2 = Room.new(:bedroom, 11, 15)
+  # binding.pry
+  assert_equal [room_2], @house.add_room(@room_2)
+  end
+
+  def test_it_has_more_rooms
+  skip
+
+  @room_1 = Room.new(:bedroom, 10, 13)
+  @room_2 = Room.new(:bedroom, 11, 15)
+  # binding.pry
+  assert_equal (room_1, room_2) @house.rooms
+  end
+
+end
