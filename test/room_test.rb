f0214e6e60d6e5cec184e526637457d0e08f327f
Phil DeLong <philjdelong@gmail.com>
2019-10-09 09:17:00 -0600

    Pass all iteration 1 tests

---
+++ test/room_test.rb
@@ -0,0 +1,35 @@
+require'minitest/pride'
+require'minitest/autorun'
+require'pry'
+require'./lib/room'
+
+class RoomTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_it_exists
+  # skip
+
+  assert_instance_of Room, @room
+  end
+
+  def test_it_has_a_category
+  # skip
+
+  assert_equal :bedroom, @room.category
+  end
+
+  def test_it_has_a_length
+  # skip
+
+  assert_equal 10, @room.length
+  end
+
+  def test_it_has_a_width
+  # skip
+
+  assert_equal 13, @room.width
+  end
+
+end
