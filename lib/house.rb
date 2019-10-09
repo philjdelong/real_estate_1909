e43f30999ab9b38bd03fb1ccbfac707f4875ff16
Phil DeLong <philjdelong@gmail.com>
2019-10-09 10:54:01 -0600

    Add default parameter, area

--- lib/house.rb
+++ lib/house.rb
@@ -1,14 +1,26 @@
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :area
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @area = 1900
   end
 
   def add_room(room)
     @rooms << room
+  end
+
+  def rooms_from_category(category)
+    x = []
+    @rooms.each do |room|
+      if category == room.category
+        x << room
+      end
+    end
+    # binding.pry
+    return x
   end
 
 end
