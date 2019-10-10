dc3d406a04f8833076460d9dd55029975c06dae9
Phil DeLong <philjdelong@gmail.com>
2019-10-10 09:26:52 -0600

    Add area method

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,32 +4,34 @@ require'pry'
 require'./lib/room'
 
 class RoomTest < Minitest::Test
+
   def setup
     @room = Room.new(:bedroom, 10, 13)
   end
 
   def test_it_exists
-  # skip
-
-  assert_instance_of Room, @room
+    # skip
+    assert_instance_of Room, @room
   end
 
   def test_it_has_a_category
-  # skip
-
-  assert_equal :bedroom, @room.category
+    # skip
+    assert_equal :bedroom, @room.category
   end
 
   def test_it_has_a_length
-  # skip
-
-  assert_equal 10, @room.length
+    # skip
+    assert_equal 10, @room.length
   end
 
   def test_it_has_a_width
-  # skip
+    # skip
+    assert_equal 13, @room.width
+  end
 
-  assert_equal 13, @room.width
+  def test_it_has_area
+    # skip
+    assert_equal 130, @room.area
   end
 
 end
